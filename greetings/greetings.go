d4f709c1cc5cceece02c578bbe01a609e6f457b8
alvin sidiq <123379611+alvinsidiq@users.noreply.github.com>
2025-01-05 09:01:31 +0700

    Update greetings.go

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,30 @@
 package greetings
-import "fmt"
+import 
+(
+    "errors"
+    "fmt"
+    "math/rand"
+    "time"
+
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	greetings := [] string{
+		"Hi, %v. Welcome!",
+        "Hello, %v! It's great to meet you.",
+        "Greetings, %v! Have a wonderful day.",
+	}
+
+	randomGreeting := greetings[rand.Intn(len(greetings))]
+
+	message := fmt.Sprintf(randomGreeting, name)
+	return message, nil
 }
